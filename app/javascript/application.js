af8636c38fae9edd18371f122fb01566f2d0a4da
Zaid <zaid.ahmed@ucalgary.ca>
2025-05-17 19:02:51 -0600

    project setup and bootstrap with landing page and post CRUD

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import { application } from "./application";
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
